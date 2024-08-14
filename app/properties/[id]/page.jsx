95bb5e3395cbee67172056f9750a10912d5115bb
Gabriel Tomsic <tomsicgabriel@gmail.com>
2024-08-14 10:23:13 -0500

    Different hooks from next/navigation

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -1,4 +1,17 @@
+'use client';
+import {
+   useRouter,
+   useParams,
+   useSearchParams,
+   usePathname,
+} from 'next/navigation';
+
 const PropertyPage = () => {
+   const router = useRouter();
+   const params = useParams();
+   const searchParams = useSearchParams();
+   const pathname = usePathname();
+   console.log(pathname);
    return <div>Proper Page</div>;
 };
 
