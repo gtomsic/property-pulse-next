78bb0df786c3b97f0ab94a48d13badbacf0f7a57
Gabriel Tomsic <gabriel@mac.local>
2024-08-20 18:39:15 -0500

    Initialize google auth

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import "@/assets/styles/globals.css"
 import Footer from "@/components/Footer"
 import Navbar from "@/components/Navbar"
+import AuthPvovider from "@/components/AuthPvovider"
 
 export const metadata = {
     title: "Property Pulse",
@@ -10,13 +11,15 @@ export const metadata = {
 
 const MainLayout = ({ children }) => {
     return (
-        <html>
-            <body>
-                <Navbar />
-                <main>{children}</main>
-                <Footer />
-            </body>
-        </html>
+        <AuthPvovider>
+            <html>
+                <body>
+                    <Navbar />
+                    <main>{children}</main>
+                    <Footer />
+                </body>
+            </html>
+        </AuthPvovider>
     )
 }
 
